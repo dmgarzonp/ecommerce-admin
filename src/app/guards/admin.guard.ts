3d74848ee934184d9cbde2423682e1c2b1c710f1
David Garzon <david.garzon@akroscorp.com>
2023-02-06 16:32:43 -0500

    Guards

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AdminService } from '../services/admin.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor( private _adminService: AdminService,
+               private _router: Router){
+
+  }
+
+
+  canActivate(): any {
+      if (!this._adminService.isAutenticated(['admin'])) {
+        this._router.navigate(['/login']);
+        return false;
+      }
+      return true;
+
+}
+}
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, observable } from "rxjs";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { JwtHelperService } from "@auth0/angular-jwt";
+
 
 import { GLOBAL } from "./GLOBAL";
 
@@ -22,6 +24,35 @@ export class AdminService {
 
   getToken(){
     return localStorage.getItem('token');
+  }
+
+  //Autenticando el Token
+  public isAutenticated( allowRoles: string[]): boolean{
+
+    const token = localStorage.getItem('token');
+
+
+    if (!token) {
+      return false;
+    }
+
+
+    try {
+      const helper = new JwtHelperService();
+      var decodedToken = helper.decodeToken(token);
+
+      console.log(decodedToken);
+
+      if (!decodedToken) {
+        console.log("NO TIENE ACCESO");
+        localStorage.removeItem('token')
+        return false;
+      }
+    } catch (error) {
+      localStorage.removeItem('token')
+      return false;
+    }
+    return allowRoles.includes(decodedToken['role']);
   }
 
 }
